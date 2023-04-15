1c61d95b01a82a274587cba25111c70398a34374
giedrius <giedriustum@gmail.com>
2023-04-15 20:41:05 +0300

    Some js generating magic.

--- app/Http/Livewire/ShowPorts.php
+++ app/Http/Livewire/ShowPorts.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class ShowPorts extends Component
 {
+    public string $message;
+
+    public function mount() {
+        $this->message = "The message for 'show ports'";
+    }
+
     public function render()
     {
         return view('livewire.show-ports');
--- resources/views/livewire/show-ports.blade.php
+++ resources/views/livewire/show-ports.blade.php
@@ -1,4 +1,10 @@
 <div>
+
+    <input wire:model.lazy="message" type="text">
+    <h1>{{ $message }}</h1>
+
     {{-- If your happiness depends on money, you will never be happy with yourself. --}}
     TODO show ports
+
+
 </div>
