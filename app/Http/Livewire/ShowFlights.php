663c5ae1bcc0af4f6d6171d132bcc0d28d6a4645
Giedrius Tumelis <giedrius@inkodus.lt>
2023-04-16 16:49:08 +0300

    flights component debug

--- app/Http/Livewire/ShowFlights.php
+++ app/Http/Livewire/ShowFlights.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 
 class ShowFlights extends Component
 {
+
+    public $laukas;
     public function render()
     {
         return view('livewire.show-flights');
--- resources/views/livewire/show-flights.blade.php
+++ resources/views/livewire/show-flights.blade.php
@@ -1,4 +1,8 @@
 <div>
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
-    TODO flights
+    TODO flights aaa
+
+    <input  type="text"  wire:model="laukas">
+
+    Laukas {{$laukas}}
 </div>
